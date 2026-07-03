a42e5eeeffdfbe6b59b209a48034e6b9d28b7665
athenasaint01 <diegonancay1@gmail.com>
2026-07-03 00:50:48 -0500

    Update web Hero Section v2

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,12 +1,22 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import WhatsAppFloat from "./WhatsAppFloat";
 import PopupBanner from "./PopupBanner";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'instant' as ScrollBehavior });
+  }, [pathname]);
+  return null;
+}
+
 export default function Layout() {
   return (
     <div className="min-h-dvh flex flex-col">
+      <ScrollToTop />
       <Header />
       <main className="flex-1">
         <Outlet />
