95879e8e23fda48eb8ff2406cd8c231ed6dc8a5a
athenasaint01 <diegonancay1@gmail.com>
2026-07-13 01:29:19 -0500

    update bg modal

--- client/src/components/PopupBanner.tsx
+++ client/src/components/PopupBanner.tsx
@@ -56,7 +56,7 @@ export default function PopupBanner() {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             transition={{ duration: 0.3 }}
-            className="fixed inset-0 bg-black/50 z-[100]"
+            className="fixed inset-0 bg-black/70 z-[100]"
             onClick={dismiss}
           />
 
